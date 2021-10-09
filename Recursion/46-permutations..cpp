2834ebb8100a989476ce5fd041d1b37e15d6cefe
TANISH MOHANTA <tanishmohanta1901@gmail.com>
2021-10-09 23:18:23 +0530

    46

---
+++ Recursion/46-permutations..cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void helper(vector<int>&nums,int i)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=i;j<nums.size();j++)
+        {
+            swap(nums[i],nums[j]);
+            helper(nums,i+1);
+            swap(nums[i],nums[j]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        helper(nums,0);
+        return ans;
+    }
+};
